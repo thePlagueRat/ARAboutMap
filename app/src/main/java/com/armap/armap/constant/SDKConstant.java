6ec6109ea2ded3a102cd48f95258cbc733ca25bb
TwoDogLee <qcodemonkey@gmail.com>
2019-01-14 15:21:25 +0800

    修改 sdk

--- app/src/main/java/com/armap/armap/constant/SDKConstant.java
+++ app/src/main/java/com/armap/armap/constant/SDKConstant.java
@@ -2,7 +2,7 @@ package com.armap.armap.constant;
 
 public class SDKConstant
 {
-    public static String QQ_LOGIN_ID = "101541974";
+    public static String QQ_LOGIN_ID = "101545646";
 
     public static String AR_APP_ID = "15322454";
     public static String AR_API_KEY = "dkLkY29MhMxi6dxToYs28ODT";
